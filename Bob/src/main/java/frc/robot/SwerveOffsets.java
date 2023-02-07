b4f887e357b57bd95fe12fcafedd5fa258f53538
LegoNick1208 <nshreve@outlook.com>
2023-02-07 16:50:02 -0500

    almost done swerve offset just needtofixreadfile

--- Bob/src/main/java/frc/robot/SwerveOffsets.java
+++ Bob/src/main/java/frc/robot/SwerveOffsets.java
@@ -1,16 +1,9 @@
 package frc.robot;
 
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.Console;
+import java.io.FileWriter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.ObjectOutputStream;
-import java.io.ObjectInputStream;
-import java.io.Serializable;
-
-import com.fasterxml.jackson.core.JsonParser;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
@@ -19,24 +12,23 @@ public class SwerveOffsets {
     private final String path = "/home/lvuser/";
     ModuleOffsets object = new ModuleOffsets();
     boolean resetOffsets = false;
-    File frontLeft = new File(path + "frontLeft.txt");
-    File frontRight = new File(path + "frontLeft.txt");
-    File backLeft = new File(path + "frontLeft.txt");
-    File backRight = new File(path + "frontLeft.txt");
+    String frontLeftPath = path + "frontLeft.txt";
+    String frontRightPath = path + "frontLeft.txt";
+    String backLeftPath = path + "frontLeft.txt";
+    String backRightPath = path + "frontLeft.txt";
 
     SwerveOffsets() {
         SmartDashboard.putBoolean("resetAngleOffsets", resetOffsets);
     }
 
-    double[] calculateOffsets(String fL, String fR, String bL, String bR){
+    double[] calculateOffsets(double fL, double fR, double bL, double bR){
         // takes the current values, assumes they should be 0, and returns an [] of the new values to set them to after storing it for futute use
-        ModuleOffsets oldOffsets = new ModuleOffsets(fL, fR, bL, bR);
         // if old offsets < 90, its closer to 0. Else if > 90 & < 270, closer to 180. Else its > 270, so closer to 360
         double[] newOffsets = {
-            oldOffsets.frontLeft < 90 ? -oldOffsets.frontLeft: oldOffsets.frontLeft > 90 & oldOffsets.frontLeft < 270 ? 180 - oldOffsets.frontLeft:360 - oldOffsets.frontLeft,
-            oldOffsets.frontRight < 90 ? -oldOffsets.frontRight: oldOffsets.frontRight > 90 & oldOffsets.frontRight < 270 ? 180 - oldOffsets.frontRight:360 - oldOffsets.frontRight,
-            oldOffsets.backLeft < 90 ? -oldOffsets.backLeft: oldOffsets.backLeft > 90 & oldOffsets.backLeft < 270 ? 180 - oldOffsets.backLeft:360 - oldOffsets.backLeft,
-            oldOffsets.backRight < 90 ? -oldOffsets.backRight: oldOffsets.backRight > 90 & oldOffsets.backRight < 270 ? 180 - oldOffsets.backRight:360 - oldOffsets.backRight
+            fL < 90 ? -fL: fL > 90 & fL < 270 ? 180 - fL:360 - fL,
+            fR < 90 ? -fR: fR > 90 & fR < 270 ? 180 - fR:360 - fR,
+            bL < 90 ? -bL: bL > 90 & bL < 270 ? 180 - bL:360 - bL,
+            bR < 90 ? -bR: bR > 90 & bR < 270 ? 180 - bR:360 - bR
         };
         writeOffsets(newOffsets);
         return newOffsets;
@@ -50,8 +42,23 @@ public class SwerveOffsets {
         object.backRight = newAngles[3];
 
         // attempt to write the new offsets to the file, catches exceptions if failure
-
-        
+        try {
+            FileWriter fLWriter = new FileWriter(frontLeftPath);
+            fLWriter.write(String.valueOf(object.frontLeft));
+            fLWriter.close();
+            FileWriter fRWriter = new FileWriter(frontRightPath);
+            fRWriter.write(String.valueOf(object.frontRight));
+            fRWriter.close();
+            FileWriter bLWriter = new FileWriter(backLeftPath);
+            bLWriter.write(String.valueOf(object.backLeft));
+            bLWriter.close();
+            FileWriter bRWriter = new FileWriter(backRightPath);
+            bRWriter.write(String.valueOf(object.backRight));
+            bRWriter.close();
+        } catch(IOException e){
+            
+        }
+         
     }
 
     String readFile(File file) throws IOException {
@@ -76,35 +83,12 @@ public class SwerveOffsets {
 
     }
 
-    void writeFile(String filePath, String toWrite) {
-
-        FileOutputStream fos = null;
-        try {
-            fos = new FileOutputStream((path + "frontLeft.txt"));
-            fos.write(toWrite.getBytes("UTF-8"));
-            fos.close();
-        } catch (IOException e) {
-            System.out.println(e);
-
-            
-            
-        }
-
-    }
-
     class ModuleOffsets {
 
         double frontLeft;
         double frontRight;
         double backLeft;
         double backRight;
-
-        ModuleOffsets(String fl, String fr, String bl, String br) {
-            frontLeft = Double.parseDouble(fl);
-            frontRight = Double.parseDouble(fr);
-            backLeft = Double.parseDouble(bl);
-            backRight = Double.parseDouble(br);
-        }
 
     }
     
