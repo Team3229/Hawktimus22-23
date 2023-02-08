3eb088343c160eb0336d302f94655256d36fb0c0
LegoNick1208 <nshreve@outlook.com>
2023-02-08 11:03:16 -0500

    finished offsets :D (I think)

--- Bob/src/main/java/frc/robot/SwerveOffsets.java
+++ Bob/src/main/java/frc/robot/SwerveOffsets.java
@@ -2,94 +2,86 @@ package frc.robot;
 
 import java.io.FileWriter;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.Scanner;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class SwerveOffsets {
 
     private final String path = "/home/lvuser/";
-    ModuleOffsets object = new ModuleOffsets();
     boolean resetOffsets = false;
     String frontLeftPath = path + "frontLeft.txt";
-    String frontRightPath = path + "frontLeft.txt";
-    String backLeftPath = path + "frontLeft.txt";
-    String backRightPath = path + "frontLeft.txt";
+    String frontRightPath = path + "frontRight.txt";
+    String backLeftPath = path + "backLeft.txt";
+    String backRightPath = path + "backRight.txt";
 
     SwerveOffsets() {
         SmartDashboard.putBoolean("resetAngleOffsets", resetOffsets);
     }
 
-    double[] calculateOffsets(double fL, double fR, double bL, double bR){
+    double[] calculateOffsets(double fL, double fR, double bL, double bR) {
         // takes the current values, assumes they should be 0, and returns an [] of the new values to set them to after storing it for futute use
         // if old offsets < 90, its closer to 0. Else if > 90 & < 270, closer to 180. Else its > 270, so closer to 360
+        // if the values passed, which are the current values, and should be zero.
+        double[] currentValues = readFiles();
         double[] newOffsets = {
-            fL < 90 ? -fL: fL > 90 & fL < 270 ? 180 - fL:360 - fL,
-            fR < 90 ? -fR: fR > 90 & fR < 270 ? 180 - fR:360 - fR,
-            bL < 90 ? -bL: bL > 90 & bL < 270 ? 180 - bL:360 - bL,
-            bR < 90 ? -bR: bR > 90 & bR < 270 ? 180 - bR:360 - bR
+            currentValues[0] + fL < 90 ? -fL: fL > 90 & fL < 270 ? 180 - fL:360 - fL,
+            currentValues[1] + fR < 90 ? -fR: fR > 90 & fR < 270 ? 180 - fR:360 - fR,
+            currentValues[2] + bL < 90 ? -bL: bL > 90 & bL < 270 ? 180 - bL:360 - bL,
+            currentValues[3] + bR < 90 ? -bR: bR > 90 & bR < 270 ? 180 - bR:360 - bR
         };
         writeOffsets(newOffsets);
         return newOffsets;
 
     }
     void writeOffsets(double[] newAngles) {
-        
-        object.frontLeft = newAngles[0];
-        object.frontRight = newAngles[1];
-        object.backLeft = newAngles[2];
-        object.backRight = newAngles[3];
+
 
         // attempt to write the new offsets to the file, catches exceptions if failure
+        // writes strings to the file, need to parse it to double once read.
         try {
             FileWriter fLWriter = new FileWriter(frontLeftPath);
-            fLWriter.write(String.valueOf(object.frontLeft));
+            fLWriter.write(String.valueOf(newAngles[0]));
             fLWriter.close();
             FileWriter fRWriter = new FileWriter(frontRightPath);
-            fRWriter.write(String.valueOf(object.frontRight));
+            fRWriter.write(String.valueOf(newAngles[1]));
             fRWriter.close();
             FileWriter bLWriter = new FileWriter(backLeftPath);
-            bLWriter.write(String.valueOf(object.backLeft));
+            bLWriter.write(String.valueOf(newAngles[2]));
             bLWriter.close();
             FileWriter bRWriter = new FileWriter(backRightPath);
-            bRWriter.write(String.valueOf(object.backRight));
+            bRWriter.write(String.valueOf(newAngles[3]));
             bRWriter.close();
         } catch(IOException e){
-            
+            System.out.println(e);
         }
          
     }
 
-    String readFile(File file) throws IOException {
-
-        FileInputStream fis = null;
-        int len = (int) file.length();
-        byte[] bytes = new byte[len];
-
+    double[] readFiles() {
+        // returns the currently stored values, 0,0,0,0 if none, as doubles.
+        double[] values = {0,0,0,0};
         try {
+            File fLF = new File(frontLeftPath);
+            File fRF = new File(frontRightPath);
+            File bLF = new File(backLeftPath);
+            File bRF = new File(backRightPath);
+            Scanner fLFR = new Scanner(fLF);
+            Scanner fRFR = new Scanner(fRF);
+            Scanner bLFR = new Scanner(bLF);
+            Scanner bRFR = new Scanner(bRF);
+            values[0] = Double.parseDouble(fLFR.nextLine());
+            values[1] = Double.parseDouble(fRFR.nextLine());
+            values[2] = Double.parseDouble(bLFR.nextLine());
+            values[3] = Double.parseDouble(bRFR.nextLine());
+            fLFR.close();
+            fRFR.close();
+            bLFR.close();
+            bRFR.close();
+        } catch(IOException e){
 
-            fis = new FileInputStream(file);
-            assert len == fis.read(bytes);
-
-        } catch (IOException e) {
-            fis.close();
-            file.createNewFile();
-            throw e;
         }
-
-        fis.close();
-        return new String(bytes, "UTF-8");
-
+        return values;
     }
-
-    class ModuleOffsets {
-
-        double frontLeft;
-        double frontRight;
-        double backLeft;
-        double backRight;
-
-    }
-    
 }
