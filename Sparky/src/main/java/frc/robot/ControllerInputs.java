d181d23029edc52063664a01cc9723bbed7684e9
LegoNick1208 <nshreve@outlook.com>
2023-04-23 12:31:40 -0400

    Rewrote controller inputs
    Rewrote controller inputs, did not hook it up to robot.java because I have no internet and am on a plane.

--- Sparky/src/main/java/frc/robot/ControllerInputs.java
+++ Sparky/src/main/java/frc/robot/ControllerInputs.java
@@ -3,38 +3,19 @@ package frc.robot;
 import java.io.Serializable;
 
 public class ControllerInputs implements Serializable {
-    // Driver Controls
-    double d_rightY;
-    double d_rightX;
-    double d_leftX;
-    double d_leftY;
-    boolean d_AButton;
-    boolean d_BButton;
-    boolean d_XButton;
-    boolean d_YButton;
-    boolean d_RightBumper;
-    boolean d_LeftBumper;
-    double d_RightTriggerAxis;
-    double d_LeftTriggerAxis;
-    int d_POV;
-    boolean d_StartButton;
-    boolean d_BackButton;
-  
-    // Manip Controls
-    double m_rightY;
-    double m_rightX;
-    double m_leftX;
-    double m_leftY;
-    boolean m_AButton;
-    boolean m_BButton;
-    boolean m_XButton;
-    boolean m_YButton;
-    boolean m_RightBumper;
-    boolean m_LeftBumper;
-    boolean m_AButtonPressed;
-    double m_RightTriggerAxis;
-    double m_LeftTriggerAxis;
-    int m_POV;
-    boolean m_StartButton;
-    boolean m_BackButton;
+    double rightY;
+    double rightX;
+    double leftX;
+    double leftY;
+    boolean AButton;
+    boolean BButton;
+    boolean XButton;
+    boolean YButton;
+    boolean RightBumper;
+    boolean LeftBumper;
+    double RightTriggerAxis;
+    double LeftTriggerAxis;
+    int POV;
+    boolean StartButton;
+    boolean BackButton;
   }
---
+++ Sparky/src/main/java/frc/robot/Inputs.java
@@ -0,0 +1,72 @@
+// Author: Tony Simone (3229 Mentor)
+
+// Controller class
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.XboxController;
+
+public class Inputs {
+  //Limits
+  static final double STICK_DEADBAND = 0.125;
+  static final double TRIGGER_DEADBAND = 0.1;
+
+  // Controller
+  private XboxController[] Controllers = {null,null,null,null,null};
+  private GenericHID[] Rumbles = {null,null,null,null,null};
+  public int controllerCount = 0;
+  public ControllerInputs[] ControllerInputs = {null,null,null,null,null};
+  GenericHID m_rumble;
+  GenericHID d_rumble;
+
+  public Inputs(int count) {
+    controllerCount = count;
+    for(int i = 0; i < count; i++){
+      Controllers[i] = new XboxController(i);
+      Rumbles[i] = new GenericHID(i);
+      ControllerInputs[i] = new ControllerInputs();
+    }
+  }
+
+  void getControls() {
+    //Gets controls for each assigned controller
+    for(var i = 0; i < controllerCount; i++){
+      ControllerInputs[i].rightY = ((Math.abs(Controllers[i].getRightY()) < STICK_DEADBAND) ? 0 : Controllers[i].getRightY());
+      ControllerInputs[i].rightX = ((Math.abs(Controllers[i].getRightX()) < STICK_DEADBAND) ? 0 : Controllers[i].getRightX());
+      ControllerInputs[i].leftY = ((Math.abs(Controllers[i].getLeftY()) < STICK_DEADBAND) ? 0 : Controllers[i].getLeftY());
+      ControllerInputs[i].leftX = ((Math.abs(Controllers[i].getLeftX()) < STICK_DEADBAND) ? 0 : Controllers[i].getLeftX());
+      ControllerInputs[i].AButton = Controllers[i].getAButton();
+      ControllerInputs[i].BButton = Controllers[i].getBButton();
+      ControllerInputs[i].XButton = Controllers[i].getXButton();
+      ControllerInputs[i].YButton = Controllers[i].getYButton();
+      ControllerInputs[i].StartButton = Controllers[i].getStartButton();
+      ControllerInputs[i].BackButton = Controllers[i].getBackButton();
+      ControllerInputs[i].RightBumper = Controllers[i].getRightBumper();
+      ControllerInputs[i].LeftBumper = Controllers[i].getLeftBumper();
+      ControllerInputs[i].RightTriggerAxis = ((Math.abs(Controllers[i].getRightTriggerAxis()) < TRIGGER_DEADBAND) ? 0 : Controllers[i].getRightTriggerAxis());
+      ControllerInputs[i].LeftTriggerAxis = ((Math.abs(Controllers[i].getLeftTriggerAxis()) < TRIGGER_DEADBAND) ? 0 : Controllers[i].getLeftTriggerAxis());
+      ControllerInputs[i].POV = Controllers[i].getPOV();
+    }
+  }
+
+  public void nullControls() {
+    for(var i = 0; i < controllerCount; i++){
+      ControllerInputs[i].rightY = 0;
+      ControllerInputs[i].rightX = 0;
+      ControllerInputs[i].leftY = 0;
+      ControllerInputs[i].leftX = 0;
+      ControllerInputs[i].AButton = false;
+      ControllerInputs[i].BButton = false;
+      ControllerInputs[i].XButton = false;
+      ControllerInputs[i].YButton = false;
+      ControllerInputs[i].StartButton = false;
+      ControllerInputs[i].BackButton = false;
+      ControllerInputs[i].RightBumper = false;
+      ControllerInputs[i].LeftBumper = false;
+      ControllerInputs[i].RightTriggerAxis = 0;
+      ControllerInputs[i].LeftTriggerAxis = 0;
+      ControllerInputs[i].POV = -1;
+    }
+  }
+}
